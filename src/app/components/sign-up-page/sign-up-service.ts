be8d54eefc3eb3c35b6c48854b974c8c6d5e1c91
Saurabh <saurabh.jadhav@extentia.com>
2021-09-11 13:31:20 +0530

    second commit

--- src/app/components/sign-up-page/sign-up-service.ts
+++ src/app/components/sign-up-page/sign-up-service.ts
@@ -1,36 +1,36 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+// import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { catchError } from 'rxjs/operators';
+// import { throwError } from 'rxjs';
 
-export interface AuthResponse {
-    idToken:string;
-    email:string;
-    refreshToken:string;
-    expiresIn:string;
-    localId:string;
-    registeredId?:string;
-}
+// export interface AuthResponse {
+//     idToken:string;
+//     email:string;
+//     refreshToken:string;
+//     expiresIn:string;
+//     localId:string;
+//     registeredId?:string;
+// }
 
-@Injectable({providedIn:'root'})
-export class SignUpService {
+// @Injectable({providedIn:'root'})
+// export class SignUpService {
 
-    constructor(private http: HttpClient){}
+//     constructor(private http: HttpClient){}
 
-    signup(email:string,pwd:string){
-        return this.http.post<AuthResponse>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDtuKNh_fMpMGDmaqLEXYtYRgZan6IJmOo',
-            {
-                email: email,
-                passwprd:pwd,
-                returnSecureToken: true
-            }
-        ).pipe(catchError(this.handleError));
-    }
+//     signup(email:string,pwd:string){
+//         return this.http.post<AuthResponse>(
+//             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDtuKNh_fMpMGDmaqLEXYtYRgZan6IJmOo',
+//             {
+//                 email: email,
+//                 passwprd:pwd,
+//                 returnSecureToken: true
+//             }
+//         ).pipe(catchError(this.handleError));
+//     }
 
-    private handleError(errorRes: HttpErrorResponse){
-        let errorMessage = 'testtt';
-        return errorMessage;
-    }
-}
+//     private handleError(errorRes: HttpErrorResponse){
+//         let errorMessage = 'testtt';
+//         return errorMessage;
+//     }
+// }
 
